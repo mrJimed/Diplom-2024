4305c26499167e3093e7e53304d3660eef09ee8e
Danil <danil0316@mail.ru>
2024-02-19 18:24:58 +0500

    update main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from db_models import db
 
 from controllers.summarization_controller import summarization_controller
 
 app = Flask(__name__)
 app.register_blueprint(summarization_controller)
 
+db.init_app(app)
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5050)
