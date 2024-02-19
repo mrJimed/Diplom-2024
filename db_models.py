9683521941ecd8b40c6e6dbcf7f3b4855cbe51d6
Danil <danil0316@mail.ru>
2024-02-19 18:23:33 +0500

    add db models

---
+++ db_models.py
@@ -0,0 +1,24 @@
+import uuid
+from datetime import datetime
+
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4())
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    summarized_texts = db.relationship('SummarizedText', backref='user', lazy=True)
+
+
+class SummarizedText(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4())
+    title = db.Column(db.Text, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    create_ts = db.Column(db.TIMESTAMP, nullable=False, default=datetime.now)
