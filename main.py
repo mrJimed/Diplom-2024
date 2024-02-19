81cd39c2d7ec421507c972fc30b298342e40ffcb
Danil <danil0316@mail.ru>
2024-02-19 18:35:58 +0500

    add db connection

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask
 from db_models import db
 
 from controllers.summarization_controller import summarization_controller
+from settings import SECRET_KEY, DB_CONNECTION
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION
+
 app.register_blueprint(summarization_controller)
 
 db.init_app(app)
---
+++ settings.py
@@ -0,0 +1,6 @@
+from dotenv import dotenv_values
+
+env_vars = dotenv_values('.config.env')
+
+DB_CONNECTION =env_vars.get('DB_CONNECTION')
+SECRET_KEY = env_vars.get('SECRET_KEY')
