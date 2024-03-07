61a736095cc3f8bd33bfdb53cfdae2ece890a5f1
Danil <danil0316@mail.ru>
2024-03-07 20:32:32 +0500

    update settings

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from dotenv import dotenv_values
 
 env_vars = dotenv_values('.config.env')
 
-DB_CONNECTION =env_vars.get('DB_CONNECTION')
+DB_CONNECTION = env_vars.get('DB_CONNECTION')
 SECRET_KEY = env_vars.get('SECRET_KEY')
