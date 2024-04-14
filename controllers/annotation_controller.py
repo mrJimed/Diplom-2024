9605d97f4d0302cfdd2b07f0d51099b02dfffa99
Danil <danil0316@mail.ru>
2024-04-14 18:56:13 +0500

    update annotation_controller

--- controllers/annotation_controller.py
+++ controllers/annotation_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from werkzeug.datastructures.file_storage import FileStorage
 
 from services import email_service
 from summarization.abstractive.t5_summarizer import T5Summarizer as t5
@@ -7,17 +8,22 @@ from summarization.extractive.lex_rank_summarizer import LexRankSummarizer as le
 annotation_controller = Blueprint('annotation_controller', __name__, url_prefix='/annotation')
 
 
+def get_file_content(file: FileStorage):
+    return file.stream.read().decode("utf-8")
+
+
 @annotation_controller.post('/extractive')
 def extractive():
-    data = request.get_json()
-    text = str(data['text'])
-    top_n = int(data['topN'])
+    file = request.files['file']
+    top_n = int(request.form['topN'])
+    text = get_file_content(file)
     annotation = lexrank.summarize_text(text, top_n, False)
-    if bool(data['isSendEmail']):
-        to_email = data['toEmail']
+
+    if request.form['isSendEmail'].lower() == 'true':
+        to_email = request.form['toEmail']
         email_service.send_email(
             to_email=to_email,
-            subject='Результат аннотирования',
+            subject=f'Результат аннотирования для файла \"{file.filename}\"',
             text=annotation
         )
     return annotation
@@ -25,14 +31,15 @@ def extractive():
 
 @annotation_controller.post('/abstractive')
 def abstractive():
-    data = request.get_json()
-    text = str(data['text'])
+    file = request.files['file']
+    text = get_file_content(file)
     annotation = t5().summarize_text(text)
-    if bool(data['isSendEmail']):
-        to_email = data['toEmail']
+
+    if request.form['isSendEmail'].lower() == 'true':
+        to_email = request.form['toEmail']
         email_service.send_email(
             to_email=to_email,
-            subject='Результат аннотирования',
+            subject=f'Результат аннотирования для файла \"{file.filename}\"',
             text=annotation
         )
     return annotation
