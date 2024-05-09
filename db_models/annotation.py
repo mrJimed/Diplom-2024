a633b9f671e0fc9ffa7436242472e2bf02af4cf0
Danil <danil0316@mail.ru>
2024-05-09 11:39:08 +0500

    change title type

--- db_models/annotation.py
+++ db_models/annotation.py
@@ -9,7 +9,7 @@ from database import db
 class Annotation(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4())
     create_ts = db.Column(db.TIMESTAMP, nullable=False, default=datetime.now)
-    title = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String, nullable=False)
     text = db.Column(db.Text, nullable=False)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
 
