14cff834ed79d4e3105edf27adc3ae2ea3125926
Danil <danil0316@mail.ru>
2024-05-10 17:27:09 +0500

    added transcribation in controller

--- transcribation/demo.py
+++ transcribation/demo.py
@@ -2,15 +2,14 @@
 Демонстрация использования модуля SpeechRecognitionModule.
 """
 import torch
+
 from SpeechRecognitionModule import speech2text
 
-# Setting up parameters
-path2audio = "path/to/your/audio" # тут путь прям к файлу типа audio.avi или как там, ну ты понял
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-verbose = 1  # 0 = just output, 1 = output + time stats, 2 = output + time stats + all in-between outputs
-#verbose лучше 0 ставить, 1 для тестов
-# Getting text from audio
-output_text = speech2text(path2audio,
-                          device,
-                          verbose)
-print(output_text)
+
+def transcribation(path2audio: str):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    verbose = 1  # 0 = just output, 1 = output + time stats, 2 = output + time stats + all in-between outputs
+    # verbose лучше 0 ставить, 1 для тестов
+    return speech2text(path2audio,
+                       device,
+                       verbose)
